b45ab07f165afa7e83ebf3299cacaeb472d3f88b
Avinash Kumar <expandableavinash@gmail.com>
2023-08-21 13:13:47 +0530

    Enhanse : completed the website

--- js/index.js
+++ js/index.js
@@ -8,3 +8,32 @@ hamburger.addEventListener('click',function () {
     menu.classList.toggle('is-active');
     
 })
+
+const changeButton = document.querySelectorAll('.change-button')
+const changeButtonTwo = document.querySelectorAll('.change-button-two')
+const card = document.querySelectorAll('.patent-card-one')
+const cardTwo = document.querySelectorAll('.patent-card-two')
+console.log(card,card)
+
+for (let i = 0; i < changeButton.length; i++) {
+    changeButton[i].addEventListener('click',function(){
+        card[i].classList.toggle('move')
+        cardTwo[i].classList.toggle('move')
+    });
+}
+
+for (let i = 0; i < changeButtonTwo.length; i++) {
+    changeButtonTwo[i].addEventListener('click',function(){
+        cardTwo[i].classList.toggle('move')
+        card[i].classList.toggle('move')
+    });
+}
+
+// card.addEventListener('click',function () {
+//     this.classList.toggle('move');
+//     cardTwo.classList.toggle('move');
+// })
+// cardTwo.addEventListener('click',function ($this) {
+//     this.classList.toggle('move');
+//     card.classList.toggle('move');
+// })
